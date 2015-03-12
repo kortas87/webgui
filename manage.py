145eacf1221c6e7c315c881101c192df98c07872
marcel@beltz.info <marcel@beltz.info>
2015-03-12 15:27:10 +0100

    Server config

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ from app.bmslion import BmsLion
 import time
 
 
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -26,7 +26,7 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 manager.add_command("runserver", Server(
     use_debugger = True,
-    use_reloader = True)
+    use_reloader = True))
 
 if __name__ == '__main__':
     # run bms thread with serial com
